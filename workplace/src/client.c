3bc8531ae8a36f27e7ec6693c57e88bd2c8aae05
Jikuhara <kei2003730@gmail.com>
2025-07-24 16:56:46 +0900

    save

--- workplace/src/client.c
+++ workplace/src/client.c
@@ -3,42 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kei2003730 <kei2003730@student.42.fr>      +#+  +:+       +#+        */
+/*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 14:49:05 by kei2003730        #+#    #+#             */
-/*   Updated: 2025/07/04 19:04:02 by kei2003730       ###   ########.fr       */
+/*   Updated: 2025/07/07 20:52:05 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
-// int	main(int argc, char *argv[])
-// {
-// 	pid_t server_pid;
-
-// 	server_pid = atoi(argv[1]);
-// 	printf("parent PID = %i\n", server_pid);
-// 	kill(server_pid, SIGALRM); /* なに送信したら良いか分からん。 */
-// 	return (0);
-// }
-
-// int main(int argc, char const *argv[])
-// {
-// 	int num;
-
-// 	for (size_t i = 0; i < argc; i++)
-// 	{
-// 		for (size_t j = 0; j < strlen(argv[i]); j++)
-// 		{
-// 			num = argv[i][j];
-// 			printf("argv[%zu][%zu] = %i (%c)\n", i , j, num, argv[i][j]);
-// 		}
-// 		printf("\n");
-// 	}
-// 	return 0;
-// }
-
-void send_char(pid_t server_pid, char c)
+int send_char(pid_t server_pid, char c)
 {
 	int bit;
 
@@ -47,26 +21,68 @@ void send_char(pid_t server_pid, char c)
 		bit = (c >> i) & 1;
 		if (bit == 0)
 		{
-			kill(server_pid, SIGUSR1);
+			if (kill(server_pid, SIGUSR1) == -1)
+			{
+				return (-1);
+			}
 		}
 		else
 		{
-			kill(server_pid, SIGUSR2);
+			if (kill(server_pid, SIGUSR2) == -1)
+			{
+				return (-1);
+			}
 		}
 		usleep(100);
 	}
+	return (0);
 }
 
-void send_string(pid_t server_pid, char *str)
+int send_string(pid_t server_pid, char *str)
 {
 	int i = 0;
 
 	while (str[i])
 	{
-		send_char(server_pid, str[i]);
+		if (send_char(server_pid, str[i]) == -1)
+		{
+			return (-1);
+		}
 		i++;
 	}
-	send_char(server_pid, '\0');
+	if (send_char(server_pid, '\0') == -1)
+	{
+		return (-1);
+	}
+	return (0);
+}
+
+int	send_client_pid(pid_t server_pid)
+{
+	pid_t client_pid;
+	int bit;
+
+	client_pid = get_pid();
+	for (int i = 0; i < 32; i++)
+	{
+		bit = (client_pid >> i) & 1;
+		if (bit == 0)
+		{
+			if (kill(server_pid, SIGUSR1) == -1)
+			{
+				return (-1);
+			}
+		}
+		else
+		{
+			if (kill(server_pid, SIGUSR2) == -1)
+			{
+				return (-1);
+			}
+		}
+		usleep(100);
+	}
+	return (0);
 }
 
 int main(int argc, char const *argv[])
@@ -84,7 +100,13 @@ int main(int argc, char const *argv[])
 		printf("Error: Invalid server PID\n");
 		return (1);
 	}
-	send_string(server_pid, (char *)argv[2]);
-
+	if (send_client_pid(server_pid) == -1)
+	{
+		return (-1);
+	}
+	if (send_string(server_pid, (char *)argv[2]) == -1)
+	{
+		return (-1);
+	}
 	return (0);
 }
